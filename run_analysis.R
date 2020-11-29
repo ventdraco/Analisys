ad6523c2fe0b01fc3ddd17bfc7c212b816cbfed1
ventdraco <ventdraco@gmail.com>
2020-11-28 20:56:21 -0800

    reading train data

--- run_analysis.R
+++ run_analysis.R
@@ -20,6 +20,15 @@ features <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
 ##features_info contain info about features table, not necessary for this assignment
 ## reading test data
 subtest <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
-unique(subtest, incomparables = FALSE) ## identified unique codes for subject_test table [1] "2"  "4"  "9"  "10" "12" "13" "18" "20" "24"
+lapply(subtest,unique) ## identified unique codes for subject_test table [1] "2"  "4"  "9"  "10" "12" "13" "18" "20" "24"
+xtest <- read.table("UCI HAR Dataset/test/x_test.txt", header = FALSE) ## info
+test <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)  ## activity id
+lapply(ytest, unique) ##checking unique codes for y_test list was 5 4 6 1 3 2
+##reading train data
+subtrain <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+lapply(subtrain,unique) ## identified unique codes for subject_train table [1]  1  3  5  6  7  8 11 14 15 16 17 19 21 22 23 25 26 27 28 29 30
+xtrain <- read.table("UCI HAR Dataset/train/x_train.txt", header = FALSE) ## info
+ytrain <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)  ## activity id
+lapply(ytrain, unique) ##checking unique codes for y_test list was 5 4 6 1 3 2
 
 
